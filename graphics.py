7a8ae179481879c4504483647e1091140c71a959
Ramesh Gehlot <rameshgehlot2210@gmail.com>
2026-06-26 13:44:23 +0530

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -1,13 +1,1 @@
-# 🌀Spiral Fractal Pattern (Turtle Graphics) 
-import turtle
 
-t = turtle.Turtle()
-t.speed(0)
-t.color("cyan")
-turtle.bgcolor("black")
-
-for i in range(200):
-    t.forward(i * 2)
-    t.left(59)
-
-turtle.done()
